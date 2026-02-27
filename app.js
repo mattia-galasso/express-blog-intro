cfa9b1b1e82f3014df4ee1bacbaeaffba83eb90f
MattiaGalasso <galasso.mattia94@gmail.com>
2026-02-27 19:58:12 +0100

    Add: first app.get

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const app = express();
 const port = 3000;
 const appURL = `http://localhost:${port}`;
 
-//*
+//* Middlewave Assets
+
+//* Routes
+app.get("/", (req, res) => {
+  console.log("Server del mio blog");
+});
